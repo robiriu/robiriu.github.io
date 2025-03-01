437b1a1cbaf7fec756d107a9d326881425131523
robiriu <robiriu@gmail.com>
2025-03-01 15:59:37 +0700

    Explore Section to Show Other Pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Hamburger Menu Toggle
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('header nav');
-  
-    hamburger.addEventListener('click', () => {
-      nav.classList.toggle('active');
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector("header nav");
+
+  hamburger.addEventListener("click", function() {
+    nav.classList.toggle("active");
   });
+});
